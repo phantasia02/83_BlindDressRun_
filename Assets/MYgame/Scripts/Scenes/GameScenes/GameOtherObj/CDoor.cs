69a6cec614d553436f24e2bfe47a52e648eebefa
phantasia02 <https://github.com/phantasia02/UsavichSolitaireForTaiwan>
2021-08-17 16:42:47 +0800

    Add Door  RoleAccessories

--- Assets/MYgame/Scripts/Scenes/GameScenes/GameOtherObj/CDoor.cs
+++ Assets/MYgame/Scripts/Scenes/GameScenes/GameOtherObj/CDoor.cs
@@ -5,6 +5,8 @@ using DG.Tweening;
 
 public class CDoor : MonoBehaviour
 {
+
+    [SerializeField] protected AnimationCurve m_MyAnimationCurve;
     [SerializeField] protected Renderer     m_DoorRenderer          = null;
     [SerializeField] protected Renderer     m_AccessoriesRenderer   = null;
     [SerializeField] protected GameObject   m_ShowAccessories       = null;
@@ -22,6 +24,19 @@ public class CDoor : MonoBehaviour
 
         m_DoorRenderer.material         = lTempCGGameSceneData.m_AllDoorMat[(int)m_MyDoorType];
         m_AccessoriesRenderer.material  = lTempCGGameSceneData.m_AllPlayAccessoriesMat[(int)m_MyPlayAccessoriesType];
+
+
+        GameObject lTempGameObject = GameObject.Instantiate(m_ShowAccessories, this.transform);
+        Transform lTempTransform = lTempGameObject.transform;
+        Vector3 lTempV3 = this.transform.position;
+        lTempV3.y += 2.5f;
+        lTempGameObject.transform.position = lTempV3;
+
+        Tween lTempTween = lTempTransform.DORotate(new Vector3(0.0f, 360.0f, 0.0f), 1.0f, RotateMode.LocalAxisAdd).SetEase(Ease.Linear);
+        lTempTween.SetLoops(-1, LoopType.Restart);
+        lTempTween = lTempTransform.DOLocalMoveY( 3.0f, 2.0f).SetEase(m_MyAnimationCurve);
+        lTempTween.SetLoops(-1, LoopType.Restart);
+
     }
 
     public void ShowAccessories(bool show){m_AccessoriesRenderer.gameObject.SetActive(show);}
